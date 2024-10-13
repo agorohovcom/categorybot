acc4f4ef23a7358b9b0a5173a0e5665f994e907f
agorohov <aleksandr.gorohov@gmail.com>
2024-10-13 10:47:58 +0300

    init commit

---
+++ src/test/java/com/agorohov/categorybot/CategoryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agorohov.categorybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
